960f7492649ab5920eb19602d3f1d8c419768189
Thien Nguyen <ngocthienTCVan.b10@gmail.com>
2021-05-05 21:23:21 -0700

    init project

---
+++ addons/openeducat_assignment/models/assignment_type.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of OpenEduCat. See LICENSE file for full copyright & licensing details.
+
+##############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech Receptives(<http://www.techreceptives.com>).
+#
+##############################################################################
+
+from odoo import models, fields
+
+
+class GradingAssigmentType(models.Model):
+    _name = 'grading.assignment.type'
+    _description = "Assignment Type"
+
+    name = fields.Char(string="Name", required=True)
+    code = fields.Char(string="Code")
+    assign_type = fields.Selection([('sub', 'Subjective'),
+                                    ('attendance', 'Attendance')],
+                                   string='Type', default='sub')
